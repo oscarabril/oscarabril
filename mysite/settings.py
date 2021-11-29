603720bb7f5fd06b32be695a7cb2890dcbf7e3e2
Oscar Abril <oscar.4bril@icloud.com>
2021-11-29 15:56:38 +0000

    .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,19 +11,31 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)0r_gk$aavnfgf9d!n_-^wqzs&1)drjo%sh$ipbj)q!y134co'
+#SECRET_KEY = 'django-insecure-3)0r_gk$aavnfgf9d!n_-^wqzs&1)drjo%sh$ipbj)q!y134co'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
